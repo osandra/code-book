2f707b8b3d95ae750d32fcd3a77614c454f12d4f
osandra <okhw97@gmail.com>
2022-09-21 22:14:42 +0900

    Add initial setting

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	val springBootVersion = "2.7.1"
+	val kotlinVersion = "1.6.21"
+	id("org.springframework.boot") version springBootVersion
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+allprojects {
+	group = "code-book"
+	version = "0.0.1-SNAPSHOT"
+
+	repositories {
+		mavenCentral()
+	}
+}
+
+subprojects {
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+	dependencies {
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "17"
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "code-book"
+
+include(
+    "book",
+    "api"
+)
